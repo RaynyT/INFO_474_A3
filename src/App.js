d64b14b884e5e42950e3e610292d7fd7ec104226
Akoly Vongdala <akolyv@uw.edu>
2021-05-09 15:52:32 -0700

    Added information to the webpage

--- src/App.js
+++ src/App.js
@@ -8,9 +8,30 @@ const App = () => {
         "https://raw.githubusercontent.com/RaynyT/INFO_474_A3/main/data/Data.csv"
     );
     
-    console.log(data)
+    // defining constants 
+    const chartSize = 500;
+    const margin = 20;
+
+    console.log("from hook", loading, data);
+
     return(
-        <h1>hello world!</h1>
+        <div className="vis">
+            <p>{loading && "Loading data!"}</p>
+            {/* Introduction */}
+            <h3> Dataset: Unemployment rates for persons 25 years and older by educational attainment</h3>
+            <p> About the dataset: place holder</p>
+            <p> Analysis questions: place holder</p>
+
+            {/* Visualization */}
+            <h3>Visualization name goes here</h3>
+            <svg width={chartSize + 500} height={chartSize} style={{border : "1px solid black"}}> 
+
+            </svg>
+
+
+        </div>
+
+
     );
 
 };
